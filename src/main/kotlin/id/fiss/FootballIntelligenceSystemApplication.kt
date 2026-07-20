954a1e26bb226f1f024a6391d25e854d5e72ab40
Ari Dinata <ari08dinata@gmail.com>
2026-07-20 22:08:54 +0800

    chore: recreate FISS engineering baseline

---
+++ src/main/kotlin/id/fiss/FootballIntelligenceSystemApplication.kt
@@ -0,0 +1,11 @@
+package id.fiss
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FootballIntelligenceSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<FootballIntelligenceSystemApplication>(*args)
+}
